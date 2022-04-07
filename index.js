232382bb624525c27ede0e68db94e2787454d42e
Uriel Escorcia <uris_ec@hotmail.com>
2022-04-07 02:54:57 -0500

    Server Ready

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path')
+const { setupIO } = require('./sockets/socket')
 require('dotenv').config()
 
 const app = express()
 
 const server = require('http').createServer(app)
-module.exports.io = io = require('socket.io')(server)
+const io = require('socket.io')(server)
+
+setupIO(io)
 
 const publicPath = path.resolve(__dirname, 'public')
 app.use(express.static(publicPath))
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,42 @@
-const { io } = require('../index')
+const Bands = require('../models/bands')
+const Band = require('../models/band')
 
-io.on('connection', (client) => {
-    console.log('Client connected')
+const bands = new Bands()
+bands.addBand(new Band('Queen'))
+bands.addBand(new Band('Led Zeppelin'))
+bands.addBand(new Band('Pink Floyd'))
+bands.addBand(new Band('The Rolling Stones'))
+bands.addBand(new Band('The Who'))
 
-    client.on('disconnect', () => {
-        console.log('Client disconnected')
+console.log('init db')
+
+const setupIO = (io) => {
+    io.on('connection', (client) => {
+        console.log('Client connected')
+
+        client.emit('active-bands', bands.getBands())
+
+        client.on('disconnect', () => {
+            console.log('Client disconnected')
+        })
+
+        client.on('vote-band', (payload) => {
+            bands.voteBand(payload.id)
+            io.emit('active-bands', bands.getBands())
+        })
+
+        client.on('add-band', (payload) => {
+            console.log(payload)
+            bands.addBand(new Band(payload.name))
+            io.emit('active-bands', bands.getBands())
+        })
+
+        client.on('delete-band', (payload) => {
+            console.log(payload)
+            bands.deleteBand(payload.id)
+            io.emit('active-bands', bands.getBands())
+        })
     })
+}
 
-    client.on('message', (payload) => {
-        console.log(payload)
-
-        io.emit('message', payload)
-    })
-})
+module.exports = { setupIO }
